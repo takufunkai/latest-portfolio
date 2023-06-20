5f57a258c3aaf9ec8fff9b5d7c8a9055adb5ed46
Ezekiel <takufunkai@gmail.com>
2023-06-20 23:41:01 +0800

    Fix missing map key

--- components/experienceItem.js
+++ components/experienceItem.js
@@ -9,7 +9,13 @@ export default function ExperienceItem({
       <h2 className="text-xl">{title}</h2>
       <h3 className="mt-1 text-gray-400">{subtitle}</h3>
       <div className="mt-2">{description}</div>
-      {icons.length > 0 && <div className="flex gap-2">{icons}</div>}
+      {icons.length > 0 && (
+        <div className="flex gap-2">
+          {icons.map((icon, i) => (
+            <p key={i}>{icon}</p>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
